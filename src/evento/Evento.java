13e57ea3ed8bf44ba70fe59603239bd3a14484ae
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-08-05 15:16:21 +0200

    first commit

---
+++ src/evento/Evento.java
@@ -0,0 +1,50 @@
+package evento;
+
+import java.time.LocalDate;
+
+public class Evento {
+    private String titolo;
+
+    private LocalDate data;
+
+    private int postiTotali;
+
+    private int postiPrenotati;
+
+    //Costruttore
+    public Evento(String titolo, LocalDate data, int postiTotali) {
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
+    }
+
+    //Getters
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public int getPostiTotali() {
+        return postiTotali;
+    }
+
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    //Setters
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    
+
+}
